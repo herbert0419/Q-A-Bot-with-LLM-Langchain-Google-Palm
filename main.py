fdd230506d759c65af0a1db3238a0bad043b7071
Shobhandeb Paul <devshobhan4@gmail.com>
2023-12-10 20:37:08 +0530

    faiss index updated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_helper import get_qa_chain, create_vector_db
 
-st.title("Knowledge Q&A 🌱")
+st.title("Indian Budget Q&A 🌱")
 btn = st.button("Create Knowledgebase")
 if btn:
     create_vector_db()
