ea0eae5a09de80ae5fba6dab180e55eada39c78c
Shobhandeb Paul <55660915+herbert0419@users.noreply.github.com>
2023-12-10 23:37:24 +0530

    Update langchain_helper.py

--- langchain_helper.py
+++ langchain_helper.py
@@ -9,7 +9,9 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 load_dotenv()
 
-llm = GooglePalm(google_api_key=os.environ["GOOGLE_API_KEY"], temperature=0.2)
+GOOGLE_API_KEY = "AIzaSyAiEewSIQwdZlwaWSmY-LUzVFrvTKN9FjE"
+# llm = GooglePalm(google_api_key=os.environ["GOOGLE_API_KEY"], temperature=0.2)
+llm = GooglePalm(google_api_key=GOOGLE_API_KEY , temperature=0.2)
 
 instructor_embeddings = HuggingFaceInstructEmbeddings()
 vectordb_file_path = "faiss_index"
